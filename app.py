086922306667c7edbcc9f0b0874c2637eb573dfd
Fais Nasrullah <fais@automatalabs.co>
2022-03-23 21:32:11 +0700

    penulisan string f pada python

--- app.py
+++ app.py
@@ -1,3 +1,18 @@
 print("Hello World")
 print("hello boy")
 print("tambah lagi")
+
+
+a = 4
+b = 2
+
+hasil = a > 3
+
+print(a, '>' , 3, '=', hasil)
+
+print(f"{a} > {b} = {hasil}")
+
+x = 10
+y = 12
+
+print(f"nilai x = {x} || id = {hex(id(x))} ")
